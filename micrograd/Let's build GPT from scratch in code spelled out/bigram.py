1071be6d4faf2c50711b39ff0ed6fb2ffc85b89d
Nabeel Ali Khan <nabeel03103n@gmail.com>
2026-08-26 22:42:04 +0530

    2026-08-26 22:42:04

--- micrograd/Let's build GPT from scratch in code spelled out/bigram.py
+++ micrograd/Let's build GPT from scratch in code spelled out/bigram.py
@@ -6,9 +6,111 @@ import torch.nn.functional as F
 batch_size = 32
 block_size = 8
 max_iters = 3000
-eval_interval = 1e-2
+eval_interval = 300
 device = "cuda" if torch.cuda.is_available() else "cpu"
 eval_iters = 200
+learning_rate = 1e-2
 # ---------------------------------
 
-print(device)
+torch.manual_seed(1337)
+
+with open("input.txt") as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for ch, i in stoi.items()}
+
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: [
+    "".join(itos[c] for c in l)
+]  # decoder: take a list of integers, output a string
+
+
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i : i + block_size] for i in ix]).to(device)
+    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix]).to(device)
+    return x, y
+
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ["train", "val"]:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logtis for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, target=None):
+        # idx and targets are both (B,T) tensors of integers
+        logits = self.token_embedding_table(idx)  # (B,T,C)
+        if target is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            target = target.view(B * T)
+            loss = F.cross_entropy(logits, target)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B,T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1, :]  # Becomes (B,C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=1)  # (B,C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B,1)
+            idx = torch.cat((idx, idx_next), dim=1)  # (B,T+1)
+        return idx
+
+
+model = BigramLanguageModel(vocab_size)
+m = model.to(device)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    if iter % eval_iters == 0:
+        losses = estimate_loss()
+        print(
+            f"step {iter}: train loss {losses['train']:.4f}: val loss {losses['val']:.4f}"
+        )
+
+    xb, yb = get_batch("train")
+
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, 100)[0].tolist()))
